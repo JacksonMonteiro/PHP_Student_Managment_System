29d31eee20b918de555493f6b4daacb33d737c2b
jacksonmonteiro <infor.jackson324@gmail.com>
2021-01-30 15:29:18 -0300

    login stylesheet

--- view/login_form.php
+++ view/login_form.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="./css/login.css">
+	<title>Login</title>
 </head>
 <body>
+	<div class="orange_vector"></div>
+	<div class="white_vector"></div>
+	<div class="green_vector"></div>
+
 	<main>
 		<form action="../controller/handlers/login.php" method="POST">
+			<h1>Login</h1>
 			<label for="email">
 				Email: <br>
 				<input type="text" id="email" name="eml" />
@@ -15,9 +22,9 @@
 				<input type="password" id="password" name="pwd" />
 			</label>
 			<br>
-			<input type="submit" value="Entrar">
+			<input type="submit" value="Entrar" class="login-btn">
 			<br>
-			<a href="register_form.php">Registrar-se</a>
+			<a href="register_form.php" class="register-btn">Registrar-se</a>
 		</form>
 	</main>
 </body>
